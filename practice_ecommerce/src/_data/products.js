5dd6f1fdc12949ec06fcf7d30fff5dcd6e0861ee
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2020-11-20 17:25:58 -0600

    Updated favicon. Added new product data/images

--- practice_ecommerce/src/_data/products.js
+++ practice_ecommerce/src/_data/products.js
@@ -4,11 +4,12 @@ import importImages from '../_helpers/importImages';
 const prints = importImages(require.context('../assets/images/Prints', false, /\.(png|jpe?g|svg)$/));
 const stickers = importImages(require.context('../assets/images/Stickers', false, /\.(png|jpe?g|svg)$/));
 const promo = importImages(require.context('../assets/images/Promo', false, /\.(png|jpe?g|svg)$/));
+const bundles = importImages(require.context('../assets/images/Bundles', false, /\.(png|jpe?g|svg)$/));
 
 
 export const printData = [
     {
-        name: 'Griffindog Printable Poster',
+        name: 'Holiday Cheer Printable Poster',
         price: 6.00,
         img: [prints[0], prints[1]],
         id: 10001,
@@ -20,59 +21,69 @@ export const printData = [
         tags:['harry potter', 'eco-friendly']
     },
     {
+        name: 'Dynamite Printable Poster',
+        price: 6.00,
+        img: [prints[2], prints[3]],
+        id: 10001,
+        desc: 'Accio artwork! Are you the proud parent of a Griffindog, or a proud Griffindor yourself? Show some school spirit with this adorable Harry Potter-inspired illustration! This high-resolution artwork can be printed at home or at your local print shop in 4 sizes: 5x7, 8x10, 11x14, and 16x20. Artwork is available for download after purchase.',
+        details:[
+            'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
+        ],
+        promo: promo[1],
+        tags:['harry potter', 'eco-friendly']
+    },
+    {
+        name: 'Griffindog Printable Poster',
+        price: 6.00,
+        img: [prints[4], prints[5]],
+        id: 10001,
+        desc: 'Accio artwork! Are you the proud parent of a Griffindog, or a proud Griffindor yourself? Show some school spirit with this adorable Harry Potter-inspired illustration! This high-resolution artwork can be printed at home or at your local print shop in 4 sizes: 5x7, 8x10, 11x14, and 16x20. Artwork is available for download after purchase.',
+        details:[
+            'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
+        ],
+        promo: promo[2],
+        tags:['harry potter', 'eco-friendly']
+    },
+    {
         name: 'Hufflepup Printable Poster',
         price: 6.00,
-        img: [prints[2],prints[3]],
+        img: [prints[6],prints[7]],
         id: 10002,
         desc: 'Accio artwork! Are you the proud parent of a Hufflepup, or a proud Hufflepuff yourself? Show some school spirit with this adorable Harry Potter-inspired illustration! This high-resolution artwork can be printed at home or at your local print shop in 4 sizes: 5x7, 8x10, 11x14, and 16x20. Artwork is available for download after purchase.',
-        details:[
-            'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
-        ],
-        promo:promo[1],
-        tags:['harry potter', 'eco-friendly']
-    },
-    {
-        name: 'Ravenpaw Printable Artwork',
-        price: 6.00,
-        img: [prints[4],prints[5]],
-        id: 10003,
-        desc: 'Accio artwork! Are you the proud parent of a Ravenpaw, or a proud Ravenclaw yourself? Show some school spirit with this adorable Harry Potter-inspired illustration! This high-resolution artwork can be printed at home or at your local print shop in 4 sizes: 5x7, 8x10, 11x14, and 16x20. Artwork is available for download after purchase.',
-        details:[
-            'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
-        ],
-        promo:promo[2],
-        tags:['harry potter', 'eco-friendly']
-    },
-    {
-        name: 'Snifferin Printable Artwork',
-        price: 6.00,
-        img: [prints[6],prints[7]],
-        id: 10004,
-        desc: 'Accio artwork! Are you the proud parent of a Snifferin, or a proud Slytherin yourself? Show some school spirit with this adorable Harry Potter-inspired illustration! This high-resolution artwork can be printed at home or at your local print shop in 4 sizes: 5x7, 8x10, 11x14, and 16x20. Artwork is available for download after purchase.',
         details:[
             'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
         ],
         promo:promo[3],
         tags:['harry potter', 'eco-friendly']
     },
+    {
+        name: 'Ravenpaw Printable Artwork',
+        price: 6.00,
+        img: [prints[8],prints[9]],
+        id: 10003,
+        desc: 'Accio artwork! Are you the proud parent of a Ravenpaw, or a proud Ravenclaw yourself? Show some school spirit with this adorable Harry Potter-inspired illustration! This high-resolution artwork can be printed at home or at your local print shop in 4 sizes: 5x7, 8x10, 11x14, and 16x20. Artwork is available for download after purchase.',
+        details:[
+            'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
+        ],
+        promo:promo[4],
+        tags:['harry potter', 'eco-friendly']
+    },
+    {
+        name: 'Snifferin Printable Artwork',
+        price: 6.00,
+        img: [prints[10],prints[11]],
+        id: 10004,
+        desc: 'Accio artwork! Are you the proud parent of a Snifferin, or a proud Slytherin yourself? Show some school spirit with this adorable Harry Potter-inspired illustration! This high-resolution artwork can be printed at home or at your local print shop in 4 sizes: 5x7, 8x10, 11x14, and 16x20. Artwork is available for download after purchase.',
+        details:[
+            'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
+        ],
+        promo:promo[5],
+        tags:['harry potter', 'eco-friendly']
+    },
  
 ]
 export const stickerData = [
-    {
-        name: 'Biden Sticker Pack',
-        price: 5.50,
-        img: [stickers[0], stickers[1], stickers[2], stickers[3], stickers[4]],
-        id: 20046,
-        desc:'An election 2020 sticker set supporting Joe Biden. Buy the pack or just one sticker. Show your support by placing these stickers on your laptop, notebook, and more!',
-        details:[
-                'Sticker Pack Contains: Shades, Go Joe Go, Blue Wave, and No Malarkey',
-                'Dimensions: 2" x 2"', 
-                'Printed on glossy white sticker paper. This sticker is NOT waterproof.',
-                'Colors may vary from screen to screen.'
-            ],
-        promo:promo[4],
-        tags:['political', 'bundle']
-    },
+    
     {
         name: 'Rock and Rollerskate Glossy Sticker',
         price: 1.99,
@@ -84,7 +95,7 @@ export const stickerData = [
                 'This sticker is NOT waterproof.',
                 'Colors may vary from screen to screen.'
             ],
-        promo:promo[11],
+        promo:promo[12],
         tags:['']
     },
     {
@@ -99,7 +110,7 @@ export const stickerData = [
                 'Printed on glossy white sticker paper. This sticker is NOT waterproof.',
                 'Colors may vary from screen to screen.'
             ],
-        promo:promo[8],
+        promo:promo[10],
         tags:['']
     },
     {
@@ -113,22 +124,8 @@ export const stickerData = [
                 'Printed on glossy white sticker paper. This sticker is NOT waterproof.',
                 'Colors may vary from screen to screen.'
             ],
-        promo:promo[8],
+        promo:promo[11],
         tags:['']
-    },
-    {
-        name: 'Plant Lover Stickers | Eco-Friendly Sticker Set',
-        price: 2.00,
-        img: [stickers[11], stickers[12], stickers[13]],
-        id: 20004,
-        desc:'Are you a proud plant parent? If you\'ve decorated your house with plants, why not your notebook, planner, or laptop?',
-        details:[
-                'Dimensions: Potted Plant: 3" x 3" | Bloom: 3.75" x 2"', 
-                'Printed on recycled kraft paper. These stickers are NOT waterproof.',
-                'Colors may vary from screen to screen.'
-            ],
-        promo:promo[5],
-        tags:['eco-friendly', 'bundle']
     },
     {
         name: 'Make Life Sweeter',
@@ -141,10 +138,70 @@ export const stickerData = [
                 'Printed on glossy white sticker paper. This sticker is NOT waterproof.',
                 'Colors may vary from screen to screen.'
             ],
-        promo:promo[13],
+        promo:promo[14],
         tags:['']
     },
 
+]
+
+const bundleData = [
+    {
+        name: 'Biden Sticker Pack',
+        price: 5.50,
+        img: [bundles[0], bundles[1], bundles[2], bundles[3], bundles[4]],
+        id: 30046,
+        desc:'An election 2020 sticker set supporting Joe Biden. Buy the pack or just one sticker. Show your support by placing these stickers on your laptop, notebook, and more!',
+        details:[
+                'Sticker Pack Contains: Shades, Go Joe Go, Blue Wave, and No Malarkey',
+                'Dimensions: 2" x 2"', 
+                'Printed on glossy white sticker paper. This sticker is NOT waterproof.',
+                'Colors may vary from screen to screen.'
+            ],
+        promo:promo[7],
+        tags:['political', 'bundle']
+    },
+    {
+        name: 'Plant Lover Stickers | Eco-Friendly Sticker Set',
+        price: 2.00,
+        img: [bundles[5], bundles[6], bundles[7]],
+        id: 30001,
+        desc:'Are you a proud plant parent? If you\'ve decorated your house with plants, why not your notebook, planner, or laptop?',
+        details:[
+                'Dimensions: Potted Plant: 3" x 3" | Bloom: 3.75" x 2"', 
+                'Printed on recycled kraft paper. These stickers are NOT waterproof.',
+                'Colors may vary from screen to screen.'
+            ],
+        promo:promo[8],
+        tags:['eco-friendly', 'bundle']
+    },
+    {
+        name: 'Home Office Sticker Set',
+        price: 5.50,
+        img: [bundles[5], bundles[6], bundles[7]],
+        id: 30002,
+        desc:'Are you a proud plant parent? If you\'ve decorated your house with plants, why not your notebook, planner, or laptop?',
+        details:[
+                'Dimensions: Potted Plant: 3" x 3" | Bloom: 3.75" x 2"', 
+                'Printed on recycled kraft paper. These stickers are NOT waterproof.',
+                'Colors may vary from screen to screen.'
+            ],
+        promo:promo[9],
+        tags:['home','office', 'bundle']
+    },
+    {
+        name: 'Self-Care Sticker Set',
+        price: 5.50,
+        img: [bundles[8], bundles[9], bundles[10], bundles[11], bundles[12]],
+        id: 30003,
+        desc:'Are you a proud plant parent? If you\'ve decorated your house with plants, why not your notebook, planner, or laptop?',
+        details:[
+                'Dimensions: Potted Plant: 3" x 3" | Bloom: 3.75" x 2"', 
+                'Printed on recycled kraft paper. These stickers are NOT waterproof.',
+                'Colors may vary from screen to screen.'
+            ],
+        promo:promo[13],
+        tags:['self-care', 'bundle']
+    },
 ]
 
 
