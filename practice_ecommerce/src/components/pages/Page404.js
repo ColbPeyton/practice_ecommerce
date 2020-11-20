0139a31344918581eee8184ca3d737342921a641
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2020-11-19 23:00:21 -0600

    Added content and styling to Page404. Removed styling from Cart

--- practice_ecommerce/src/components/pages/Page404.js
+++ practice_ecommerce/src/components/pages/Page404.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-function Page404(props){
+import {NavLink} from "react-router-dom";
+
+import issue from '../../assets/images/Branding/footer_image.png'
+import '../../styles/Page404.scss';
+
+function Page404(){
     return(
         <div className='page404'>
-            404
+            <div className='page404-container'>
+                <div className='img-container'>
+                    <img src={issue} alt='something went wrong' />
+                </div>
+                <div className='btn-container'>
+                    <NavLink className='link' to='/'>
+                        <p>Let's go home</p>
+                    </NavLink>
+                </div>
+            </div>
         </div>
     )
 };
