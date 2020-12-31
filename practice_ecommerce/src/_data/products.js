7ab653fcbea622c8d163c38c671b32b9c73b456b
ColbPeyton <33738479+ColbPeyton@users.noreply.github.com>
2020-12-30 21:30:55 -0600

    Updated logic for rendering products based on filter/catagory

--- practice_ecommerce/src/_data/products.js
+++ practice_ecommerce/src/_data/products.js
@@ -18,7 +18,7 @@ export const printData = [
             'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
         ],
         promo: promo[0],
-        tags:['BTS', 'eco-friendly']
+        tags:['BTS', 'eco-friendly', 'print']
     },
     {
         name: 'Dynamite Printable Poster',
@@ -30,7 +30,7 @@ export const printData = [
             'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
         ],
         promo: promo[1],
-        tags:['BTS', 'eco-friendly']
+        tags:['BTS', 'eco-friendly', 'print']
     },
     {
         name: 'Set The Night Alight Printable Artwork',
@@ -41,8 +41,8 @@ export const printData = [
         details:[
             'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
         ],
-        promo:promo[5],
-        tags:['BTS', 'eco-friendly']
+        promo:promo[4],
+        tags:['BTS', 'eco-friendly', 'print']
     },
     {
         name: 'Griffindog Printable Poster',
@@ -54,7 +54,7 @@ export const printData = [
             'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
         ],
         promo: promo[2],
-        tags:['harry potter', 'eco-friendly']
+        tags:['harry potter', 'eco-friendly', 'print']
     },
     {
         name: 'Hufflepup Printable Poster',
@@ -66,7 +66,7 @@ export const printData = [
             'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
         ],
         promo:promo[3],
-        tags:['harry potter', 'eco-friendly']
+        tags:['harry potter', 'eco-friendly', 'print']
     },
     {
         name: 'Ravenpaw Printable Artwork',
@@ -77,8 +77,8 @@ export const printData = [
         details:[
             'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
         ],
-        promo:promo[4],
-        tags:['harry potter', 'eco-friendly']
+        promo:promo[5],
+        tags:['harry potter', 'eco-friendly', 'print']
     },
     {
         name: 'Snifferin Printable Artwork',
@@ -89,8 +89,8 @@ export const printData = [
         details:[
             'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
         ],
-        promo:promo[5],
-        tags:['harry potter', 'eco-friendly']
+        promo:promo[6],
+        tags:['harry potter', 'eco-friendly', 'print']
     },
 
  
@@ -109,7 +109,7 @@ export const stickerData = [
                 'Colors may vary from screen to screen.'
             ],
         promo:promo[12],
-        tags:['']
+        tags:['sticker']
     },
     {
         name: 'Spicy Lil\' Kimchi Glossy Sticker',
@@ -124,7 +124,7 @@ export const stickerData = [
                 'Colors may vary from screen to screen.'
             ],
         promo:promo[10],
-        tags:['']
+        tags:['sticker']
     },
     {
         name: 'Snug as a Pug Glossy Sticker',
@@ -138,7 +138,7 @@ export const stickerData = [
                 'Colors may vary from screen to screen.'
             ],
         promo:promo[11],
-        tags:['']
+        tags:['sticker']
     },
     {
         name: 'Make Life Sweeter',
@@ -152,7 +152,7 @@ export const stickerData = [
                 'Colors may vary from screen to screen.'
             ],
         promo:promo[14],
-        tags:['']
+        tags:['sticker']
     },
 
 ]
@@ -171,12 +171,12 @@ export const bundleData = [
                 'Colors may vary from screen to screen.'
             ],
         promo:promo[7],
-        tags:['political', 'bundle']
+        tags:['political', 'bundle', 'sticker' ]
     },
     {
         name: 'Plant Lover Stickers | Eco-Friendly Sticker Set',
         price: 2.00,
-        img: [bundles[5], bundles[6], bundles[7]],
+        img: [bundles[5], bundles[6], bundles[7], bundles[8]],
         id: 30001,
         desc:'Are you a proud plant parent? If you\'ve decorated your house with plants, why not your notebook, planner, or laptop?',
         details:[
@@ -185,12 +185,12 @@ export const bundleData = [
                 'Colors may vary from screen to screen.'
             ],
         promo:promo[8],
-        tags:['eco-friendly', 'bundle']
+        tags:['political', 'bundle', 'sticker', 'eco-friendly' ]
     },
     {
         name: 'Home Office Sticker Set',
         price: 5.50,
-        img: [bundles[5], bundles[6], bundles[7]],
+        img: [bundles[9], bundles[10], bundles[11], bundles[12], bundles[13]],
         id: 30002,
         desc:'Are you a proud plant parent? If you\'ve decorated your house with plants, why not your notebook, planner, or laptop?',
         details:[
@@ -199,12 +199,12 @@ export const bundleData = [
                 'Colors may vary from screen to screen.'
             ],
         promo:promo[9],
-        tags:['home','office', 'bundle']
+        tags:['political', 'bundle', 'sticker' ]
     },
     {
         name: 'Self-Care Sticker Set',
         price: 5.50,
-        img: [bundles[8], bundles[9], bundles[10], bundles[11], bundles[12]],
+        img: [bundles[14], bundles[15], bundles[16], bundles[17], bundles[18]],
         id: 30003,
         desc:'Are you a proud plant parent? If you\'ve decorated your house with plants, why not your notebook, planner, or laptop?',
         details:[
@@ -213,210 +213,15 @@ export const bundleData = [
                 'Colors may vary from screen to screen.'
             ],
         promo:promo[13],
-        tags:['self-care', 'bundle']
+        tags:['political', 'bundle', 'sticker' ]
     },
 ]
 
 
 export const completeData = [
-    {
-        name: 'Holiday Cheer Printable Poster',
-        price: 6.00,
-        img: [prints[0], prints[1]],
-        id: 10001,
-        desc: 'Spread some holiday cheer with this BTS-inspired printable poster! If you love the holidays and BTS, this is the perfect way to celebrate the season!',
-        details:[
-            'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
-        ],
-        promo: promo[0],
-        tags:['BTS', 'eco-friendly']
-    },
-    {
-        name: 'Dynamite Printable Poster',
-        price: 6.00,
-        img: [prints[2], prints[3]],
-        id: 10002,
-        desc: 'Ithink you\'re dynamite! Remember to shine your unique light every day with this BTS-inspired printable poster.',
-        details:[
-            'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
-        ],
-        promo: promo[1],
-        tags:['BTS', 'eco-friendly']
-    },
-    {
-        name: 'Set The Night Alight Printable Artwork',
-        price: 6.00,
-        img: [prints[8],prints[9]],
-        id: 10003,
-        desc: 'Whether you\'re having friends over or having a dance party with yourself, set the night alight! Inspired by the BTS song "Dynamite," this makes a great gift or home decor for BTS fans.',
-        details:[
-            'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
-        ],
-        promo:promo[5],
-        tags:['BTS', 'eco-friendly']
-    },
-    {
-        name: 'Griffindog Printable Poster',
-        price: 6.00,
-        img: [prints[4], prints[5]],
-        id: 10004,
-        desc: 'Accio artwork! Are you the proud parent of a Griffindog, or a proud Griffindor yourself? Show some school spirit with this adorable Harry Potter-inspired illustration! This high-resolution artwork can be printed at home or at your local print shop in 4 sizes: 5x7, 8x10, 11x14, and 16x20. Artwork is available for download after purchase.',
-        details:[
-            'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
-        ],
-        promo: promo[2],
-        tags:['harry potter', 'eco-friendly']
-    },
-    {
-        name: 'Hufflepup Printable Poster',
-        price: 6.00,
-        img: [prints[6],prints[7]],
-        id: 10005,
-        desc: 'Accio artwork! Are you the proud parent of a Hufflepup, or a proud Hufflepuff yourself? Show some school spirit with this adorable Harry Potter-inspired illustration! This high-resolution artwork can be printed at home or at your local print shop in 4 sizes: 5x7, 8x10, 11x14, and 16x20. Artwork is available for download after purchase.',
-        details:[
-            'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
-        ],
-        promo:promo[3],
-        tags:['harry potter', 'eco-friendly']
-    },
-    {
-        name: 'Ravenpaw Printable Artwork',
-        price: 6.00,
-        img: [prints[10],prints[11]],
-        id: 10006,
-        desc: 'Accio artwork! Are you the proud parent of a Ravenpaw, or a proud Ravenclaw yourself? Show some school spirit with this adorable Harry Potter-inspired illustration! This high-resolution artwork can be printed at home or at your local print shop in 4 sizes: 5x7, 8x10, 11x14, and 16x20. Artwork is available for download after purchase.',
-        details:[
-            'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
-        ],
-        promo:promo[4],
-        tags:['harry potter', 'eco-friendly']
-    },
-    {
-        name: 'Snifferin Printable Artwork',
-        price: 6.00,
-        img: [prints[12],prints[13]],
-        id: 10007,
-        desc: 'Accio artwork! Are you the proud parent of a Snifferin, or a proud Slytherin yourself? Show some school spirit with this adorable Harry Potter-inspired illustration! This high-resolution artwork can be printed at home or at your local print shop in 4 sizes: 5x7, 8x10, 11x14, and 16x20. Artwork is available for download after purchase.',
-        details:[
-            'Please note that this listing is for a digital download. If you would prefer to have a physical print shipped to you-hang tight! Physical prints and stickers will be available soon'
-        ],
-        promo:promo[5],
-        tags:['harry potter', 'eco-friendly']
-    },
-    {
-        name: 'Biden Sticker Pack',
-        price: 5.50,
-        img: [bundles[0], bundles[1], bundles[2], bundles[3], bundles[4]],
-        id: 30046,
-        desc:'An election 2020 sticker set supporting Joe Biden. Buy the pack or just one sticker. Show your support by placing these stickers on your laptop, notebook, and more!',
-        details:[
-                'Sticker Pack Contains: Shades, Go Joe Go, Blue Wave, and No Malarkey',
-                'Dimensions: 2" x 2"', 
-                'Printed on glossy white sticker paper. This sticker is NOT waterproof.',
-                'Colors may vary from screen to screen.'
-            ],
-        promo:promo[7],
-        tags:['political', 'bundle']
-    },
-    {
-        name: 'Plant Lover Stickers | Eco-Friendly Sticker Set',
-        price: 2.00,
-        img: [bundles[5], bundles[6], bundles[7]],
-        id: 30001,
-        desc:'Are you a proud plant parent? If you\'ve decorated your house with plants, why not your notebook, planner, or laptop?',
-        details:[
-                'Dimensions: Potted Plant: 3" x 3" | Bloom: 3.75" x 2"', 
-                'Printed on recycled kraft paper. These stickers are NOT waterproof.',
-                'Colors may vary from screen to screen.'
-            ],
-        promo:promo[8],
-        tags:['eco-friendly', 'bundle']
-    },
-    {
-        name: 'Home Office Sticker Set',
-        price: 5.50,
-        img: [bundles[5], bundles[6], bundles[7]],
-        id: 30002,
-        desc:'Are you a proud plant parent? If you\'ve decorated your house with plants, why not your notebook, planner, or laptop?',
-        details:[
-                'Dimensions: Potted Plant: 3" x 3" | Bloom: 3.75" x 2"', 
-                'Printed on recycled kraft paper. These stickers are NOT waterproof.',
-                'Colors may vary from screen to screen.'
-            ],
-        promo:promo[9],
-        tags:['home','office', 'bundle']
-    },
-    {
-        name: 'Self-Care Sticker Set',
-        price: 5.50,
-        img: [bundles[8], bundles[9], bundles[10], bundles[11], bundles[12]],
-        id: 30003,
-        desc:'Are you a proud plant parent? If you\'ve decorated your house with plants, why not your notebook, planner, or laptop?',
-        details:[
-                'Dimensions: Potted Plant: 3" x 3" | Bloom: 3.75" x 2"', 
-                'Printed on recycled kraft paper. These stickers are NOT waterproof.',
-                'Colors may vary from screen to screen.'
-            ],
-        promo:promo[13],
-        tags:['self-care', 'bundle']
-    },
-    {
-        name: 'Rock and Rollerskate Glossy Sticker',
-        price: 1.99,
-        img: [stickers[7], stickers[8], stickers[9]],
-        id: 20001,
-        desc:'Rock and roll with this cute rollerskater sticker! This sticker makes a unique addition to laptops, journals, and more!',
-        details:[
-                'Dimensions: 2" x 2" glossy white round sticker.', 
-                'This sticker is NOT waterproof.',
-                'Colors may vary from screen to screen.'
-            ],
-        promo:promo[12],
-        tags:['']
-    },
-    {
-        name: 'Spicy Lil\' Kimchi Glossy Sticker',
-        price: 1.99,
-        img: [stickers[0], stickers[1], stickers[2], stickers[3]],
-        id: 20002,
-        desc:'Are you a little on the spicy side? Show it off with this sassy kimchi sticker. Perfect for journals, notebooks, laptops, and more!',
-        details:[
-                'Dimensions: 2" x 2"', 
-                'Available in 3 colors: blue, yellow, and pink', 
-                'Printed on glossy white sticker paper. This sticker is NOT waterproof.',
-                'Colors may vary from screen to screen.'
-            ],
-        promo:promo[10],
-        tags:['']
-    },
-    {
-        name: 'Snug as a Pug Glossy Sticker',
-        price: 2.00,
-        img: [stickers[4], stickers[5]],
-        id: 20003,
-        desc:'Cold weather means more blanket snuggles! Be like this happy pug and stay warm this winter! This sticker is perfect for decorating laptops, planners/bullet journals, and more!',
-        details:[
-                'Dimensions: 2" x 2"', 
-                'Printed on glossy white sticker paper. This sticker is NOT waterproof.',
-                'Colors may vary from screen to screen.'
-            ],
-        promo:promo[11],
-        tags:['']
-    },
-    {
-        name: 'Make Life Sweeter',
-        price: 2.00,
-        img: [stickers[10], stickers[11]],
-        id: 20005,
-        desc:'Make your life a little sweeter with this adorable sticker! Remind yourself to stay positive by placing this on your laptop or journal.',
-        details:[
-                'Dimensions: 2" x 2"', 
-                'Printed on glossy white sticker paper. This sticker is NOT waterproof.',
-                'Colors may vary from screen to screen.'
-            ],
-        promo:promo[14],
-        tags:['']
-    },
+   ...printData,
+   ...bundleData,
+   ...stickerData
 ]
 
 
--- practice_ecommerce/src/components/pages/Products.js
+++ practice_ecommerce/src/components/pages/Products.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react';
 import { connect } from 'react-redux'
 
 import {currentItem} from '../../redux/actions/actions';
-import {completeData, printData, stickerData} from '../../_data/products';
+import {completeData} from '../../_data/products';
 
 import banner from '../../assets/images/Branding/Products_Banner.jpg';
 
@@ -10,33 +10,35 @@ import '../../styles/Products.scss';
 
 function Products(props){
 
-    const [catagory, setCatagory] = useState(completeData);
+    const [catagory, setCatagory] = useState('all');
     const [filter, setFilter] = useState('');
 
-    function renderProducts(catagory){
-        return catagory.map((product, index) => {
-            if(!filter){
-                return <button className='product-item' onClick={()=> updateCurrentAndLoadProductPage(product)} key={index} 
-                    style={
-                        {
-                            backgroundImage: `url(${product.img[0].default})`,
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center center'
-                        }
-                    }>  
-                </button>
+    function renderProducts(){
+        return completeData.map((product, index) => {
+            if(product.tags.includes(catagory) || catagory === 'all'){
+                if(!filter){
+                    return <button className='product-item' onClick={()=> updateCurrentAndLoadProductPage(product)} key={index} 
+                        style={
+                            {
+                                backgroundImage: `url(${product.img[0].default})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center center'
+                            }
+                        }>  
+                    </button>
+                }
+                return product.tags.includes(filter)
+                    ?<button className='product-item' onClick={()=> updateCurrentAndLoadProductPage(product)} key={index} 
+                        style={
+                            {
+                                backgroundImage: `url(${product.img[0].default})`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center center'
+                            }
+                        }>  
+                    </button>
+                    : ''
             }
-            return product.tags.includes(filter)
-                ?<button className='product-item' onClick={()=> updateCurrentAndLoadProductPage(product)} key={index} 
-                    style={
-                        {
-                            backgroundImage: `url(${product.img[0].default})`,
-                            backgroundSize: 'cover',
-                            backgroundPosition: 'center center'
-                        }
-                    }>  
-                </button>
-                : ''
         })
     }
 
@@ -64,9 +66,9 @@ function Products(props){
 
     function updateGridSizing(){
         switch(catagory){
-            case stickerData:
+            case 'sticker':
                 return 'sticker';
-            case printData:
+            case 'print':
                 return 'print';
             default:
                 return 'all'
@@ -90,9 +92,9 @@ function Products(props){
                </div>
             <div className='catagories-container'>
                 <div className='catagories'>
-                    <button className={`${renderActive(completeData, catagory)}`} onClick={() => setCatagory(completeData)}>All</button>
-                    <button className={`${renderActive(printData, catagory)}`} onClick={() => setCatagory(printData)}>Prints</button>
-                    <button className={`${renderActive(stickerData, catagory)}`} onClick={() => setCatagory(stickerData)}>Stickers</button>
+                    <button className={`${renderActive('all', catagory)}`} onClick={() => setCatagory('all')}>All</button>
+                    <button className={`${renderActive('print', catagory)}`} onClick={() => setCatagory('print')}>Prints</button>
+                    <button className={`${renderActive('sticker', catagory)}`} onClick={() => setCatagory('sticker')}>Stickers</button>
                 </div>
                 <div className='filters'>
                     <button className={`${renderActive('bundle', filter)}`} onClick={() => updateFilter('bundle') }>Bundles</button>
